3115ab5842d3616a6b6bf90af85a00a2442f3bb4
Benjamin Singer <benjaminsinger@Ben.local>
2022-01-10 19:31:01 +0000

    more styles for the individual pokemon ppage

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -48,12 +48,12 @@ function Pokemon() {
         </div>
         <div className='container relative mx-auto'>
           <img
-            className='relative right-10 -mt-20 -mb-5 md:absolute duration-500 transition-opacity lg:opacity-100 drop-shadow-2xl'
+            className='relative right-10 -mt-20 md:-right-40 lg:right-0 xl:right-80 md:absolute duration-500 transition-opacity lg:opacity-100 drop-shadow-2xl'
             src={sprites && sprites.other.home.front_default}
             style={{ maxWidth: '650px', height: 'auto' }}
             alt='PokéPicker App Icon'
           />
-          <div className='max-w-xl'>
+          <div className='max-w-xl md:pt-20'>
             <h1 className='text-5xl drop-shadow-xl md:text-7xl font-bold mb-2 text-white'>
               <span className='capitalize'>{name}</span> {'\u2728'}
             </h1>
@@ -65,7 +65,7 @@ function Pokemon() {
       </div>
       <div
         id='app-block'
-        className=' py-10 md:py-20  bg-blue-500 overflow-hidden'
+        className=' pb-10 md:pb-20  bg-blue-500 overflow-hidden px-5'
       >
         <div className='container relative mx-auto z-10'>
           <h3>Heading</h3>
@@ -91,7 +91,7 @@ function Pokemon() {
           </p>
         </div>
         <span
-          className='text-8xl font-bold relative block transform w-100 break-all md:break-normal inline-block text-white'
+          className='text-8xl font-bold relative block transform break-all md:break-normal mt-10 text-white'
           aria-hidden='true'
         >
           {names && names.map(n => `${n.name}`).join(' ')}
